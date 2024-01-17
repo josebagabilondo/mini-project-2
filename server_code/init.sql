020a75b24a81a6b856d71afda10e71127a7cc79c
josebagabilondo <93148136+josebagabilondo@users.noreply.github.com>
2024-01-17 11:58:51 +0100

    Update init.sql

--- server_code/init.sql
+++ server_code/init.sql
@@ -14,6 +14,13 @@ CREATE TABLE IF NOT EXISTS temperature_data (
     time TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS light_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    light INT,
+    idSensor INT,
+    time TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS ip_to_id (
     id INT AUTO_INCREMENT PRIMARY KEY,
     ipv6_address VARCHAR(46) NOT NULL
