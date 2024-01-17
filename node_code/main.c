2f4a524166608972ba1e005a002f36ecfb7182d5
josebagabilondo <93148136+josebagabilondo@users.noreply.github.com>
2024-01-17 11:49:19 +0100

    Update main.c

--- node_code/main.c
+++ node_code/main.c
@@ -9,10 +9,11 @@
 #include "od.h"
 #include "fmt.h"
 
-#include <math.h>
-
 #include "lpsxxx.h"
 #include "lpsxxx_params.h"
+
+#include "isl29020.h"
+#include "isl29020_params.h"
 
 #include "xtimer.h"
 
@@ -21,6 +22,7 @@
 #include "mutex.h"
 
 static lpsxxx_t lpsxxx;
+static isl29020_t isl29020;
 
 static bool _proxied = false;
 static sock_udp_ep_t _proxy_remote;
@@ -31,12 +33,6 @@ static char _last_req_path[_LAST_REQ_PATH_MAX];
 /* Counts requests sent by CLI. */
 static uint16_t req_count = 0;
 
-float temp_mean = 39.456;
-float temp_stddev = 789.2; 
-
-float pressure_mean = 992;
-float pressure_stddev = 99.2; 
-
 mutex_t coap_mutex; 
 
 #define SERVER_ADDR "2a05:d016:1e1:9194:9c47:cc16:d6ae:84a"
@@ -44,6 +40,7 @@ mutex_t coap_mutex;
 
 char temp_thread_stack[THREAD_STACKSIZE_MAIN];
 char pres_thread_stack[THREAD_STACKSIZE_MAIN];
+char light_thread_stack[THREAD_STACKSIZE_MAIN];
 
 static void _resp_handler(const gcoap_request_memo_t *memo, coap_pkt_t* pdu,
                           const sock_udp_ep_t *remote)
@@ -221,27 +218,19 @@ static void send_coap_post_request(char *server_addr, char *server_port, char *p
 
 }
 
-float generate_normal_random(float stddev) {
-    float M_PI = 3.1415926535;
-
-    // Box-Muller transform to generate random numbers with normal distribution
-    float u1 = rand() / (float)RAND_MAX;
-    float u2 = rand() / (float)RAND_MAX;
-    float z = sqrt(-2 * log(u1)) * cos(2 * M_PI * u2);
-    
-    return stddev * z;
-}
-
-float add_noise(float stddev) {
-    int num;
-    float noise_val = 0;
-    
-    num = rand() % 100 + 1; // use rand() function to get the random number
-    if (num >= 50) {
-        // Generate a random number with normal distribution based on a stddev
-        noise_val = generate_normal_random(stddev);
+void *thread_light(void *arg) {
+    (void)arg;
+    isl29020_init(&isl29020, &isl29020_params[0]);
+    while (1) {
+        int lux = isl29020_read(&isl29020);
+        char payload_data[64];
+        sprintf(payload_data, "%d", lux);
+        printf("Thread light: %d", lux);
+        send_coap_post_request(SERVER_ADDR, SERVER_PORT, "/light", payload_data);
+        xtimer_sleep(4);
     }
-    return noise_val;
+
+    return NULL;
 }
 
 void *thread_temp(void *arg) {
@@ -251,7 +240,6 @@ void *thread_temp(void *arg) {
         int16_t temp = 0;
         lpsxxx_read_temp(&lpsxxx, &temp);
         char payload_data[64];
-        temp = temp + add_noise(temp_stddev);
         sprintf(payload_data, "%i.%u", (temp / 100), (temp % 100));
         printf("Thread temperature: %i.%u\n", (temp / 100), (temp % 100));
         send_coap_post_request(SERVER_ADDR, SERVER_PORT, "/temperature", payload_data);
@@ -267,7 +255,6 @@ void *thread_pres(void *arg) {
         uint16_t pres = 0;
         lpsxxx_read_pres(&lpsxxx, &pres);
         char payload_data[64];
-        pres = pres + add_noise(pressure_stddev);
         sprintf(payload_data, "%d", pres);
         printf("Thread pressure: %d\n", pres);
         send_coap_post_request(SERVER_ADDR, SERVER_PORT, "/pressure", payload_data);
@@ -287,4 +274,7 @@ int main(void)
                   THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST,
                 thread_pres, NULL, "pres_thread");
 
+    thread_create(light_thread_stack, sizeof(light_thread_stack),
+                THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST,
+                thread_light, NULL, "light_thread");
 }
