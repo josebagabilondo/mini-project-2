bfdbbfd51db128c4f752f1e1b17aa17b32c284bf
josebagabilondo <93148136+josebagabilondo@users.noreply.github.com>
2024-01-17 12:59:47 +0100

    Update server.py

--- server_code/coap_server/server.py
+++ server_code/coap_server/server.py
@@ -133,7 +133,7 @@ async def main():
     root = resource.Site()
     root.add_resource(["test"], test_handler())
     root.add_resource(["temperature"], handler("temperature"))
-    root.add_resource(["light"], handler("light")
+    root.add_resource(["light"], handler("light"))
     root.add_resource(["pressure"], handler("pressure"))
 
 
